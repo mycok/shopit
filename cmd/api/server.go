217d401ca4d196fdf94712201bd5fbc27762fe9c
myko <awesomeme155@gmail.com>
2021-10-20 02:39:52 +0300

    feat: server graceful shutdown
    - refactor application.serve method to handle graceful server shutdowns

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,20 +1,77 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"log"
+	"net/http"
+	"time"
+	"os"
+	"os/signal"
+	"syscall"
+	"context"
+	"errors"
 )
 
 func (app *application) serve() error {
 	srv := &http.Server{
 		Addr: fmt.Sprintf(":%d", app.config.port),
 		Handler: http.DefaultServeMux,
+		ErrorLog: log.New(app.logger, "", 0),
+		IdleTimeout: time.Minute,
 	}
+
+	// Handle graceful shutdown of the server.
+	shutdownErrChan := make(chan error)
+	go func() {
+		quitChan := make(chan os.Signal, 1)
+		signal.Notify(quitChan, syscall.SIGINT, syscall.SIGTERM)
+
+		s := <-quitChan
+
+		app.logger.LogInfo("server shutting down", map[string]string{
+			"signal": s.String(),
+		})
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+		defer cancel()
+
+		// Call Shutdown() on our server, passing in the context we just made.
+		// Shutdown() will return nil if the graceful shutdown was successful, or an
+		// error (which may happen because of a problem closing the listeners, or
+		// because the shutdown didn't complete before the 5-second context deadline is hit).
+		// We relay this return value to the shutdownError channel.
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			shutdownErrChan <- err
+		}
+
+		app.logger.LogInfo("completing all background tasks", map[string]string{
+			"addr": srv.Addr,
+		})
+
+		// Wait for all running goroutines to return / exit.
+		app.wg.Wait()
+		close(shutdownErrChan)
+	}()
 
 	app.logger.LogInfo("starting server", map[string]string{
 		"addr": srv.Addr,
 		"env": app.config.env,
 	})
 
-	return srv.ListenAndServe()
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownErrChan
+	if err != nil {
+		return err
+	}
+
+	app.logger.LogInfo("server stopped", map[string]string{
+		"addr": srv.Addr,
+	})
+
+	return nil
 }
