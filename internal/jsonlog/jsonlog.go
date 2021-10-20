8cb1548b19f0098dce29cb3666de78f8d9210700
myko <awesomeme155@gmail.com>
2021-10-20 05:35:45 +0300

    chore: add makefile

--- internal/jsonlog/jsonlog.go
+++ internal/jsonlog/jsonlog.go
@@ -12,6 +12,7 @@ import (
 // Level type represents the severity level for an entry log.
 type Level int8
 
+// Level constants that denote various severity log levels.
 const (
 	LevelInfo Level = iota
 	LevelError
@@ -33,33 +34,35 @@ func (l Level) String() string {
 	}
 }
 
-
 // Logger type holds the output destination that the log entries
 // will be written to, the minimum severity level that log entries will be written for,
 // plus a mutex for coordinating the writes.
 type Logger struct {
 	out      io.Writer
 	minLevel Level
-	mu      sync.Mutex
+	mu       sync.Mutex
 }
 
-// New() returns a new Logger instance which writes log entries at or above a minimum severity
+// New returns a new Logger instance which writes log entries at or above a minimum severity
 // level to a specific output destination.
 func New(out io.Writer, minLevel Level) *Logger {
 	return &Logger{
-		out: out,
+		out:      out,
 		minLevel: minLevel,
 	}
 }
 
+// LogInfo logs informative data to stdout.
 func (l *Logger) LogInfo(message string, props map[string]string) {
 	l.log(LevelInfo, message, props)
 }
 
+// LogError logs errors to stdout.
 func (l *Logger) LogError(err error, props map[string]string) {
 	l.log(LevelError, err.Error(), props)
 }
 
+// LogFatal logs fatal errors to stdout.
 func (l *Logger) LogFatal(err error, props map[string]string) {
 	l.log(LevelFatal, err.Error(), props)
 	// Terminate the program immediately.
@@ -72,16 +75,15 @@ func (l *Logger) log(level Level, message string, props map[string]string) (int,
 	}
 
 	inputs := struct {
-		Level string `json:"level"`
-		Time string `json:"time"`
-		Message string `json:"message"`
+		Level      string            `json:"level"`
+		Time       string            `json:"time"`
+		Message    string            `json:"message"`
 		Properties map[string]string `json:"properties,omitempty"`
-		Trace string `json:"trace,omitempty"`
-
+		Trace      string            `json:"trace,omitempty"`
 	}{
-		Level: level.String(),
-		Time: time.Now().UTC().Format(time.RFC3339),
-		Message: message,
+		Level:      level.String(),
+		Time:       time.Now().UTC().Format(time.RFC3339),
+		Message:    message,
 		Properties: props,
 	}
 
