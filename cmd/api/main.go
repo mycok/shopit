f8a782d414bd1755425aa46e60f24eec496a70ac
myko <awesomeme155@gmail.com>
2021-10-21 00:41:04 +0300

    feat: mongoBD setup
    - add and configure mongoDB

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"context"
 	"flag"
-	"fmt"
 	"os"
 	"sync"
+	"time"
 
 	"github.com/mycok/shopit/internal/jsonlog"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 const version = "1.0.0"
 
 type config struct {
-	port int
-	env  string
-	db   struct {
+	port     int
+	env      string
+	dbClient struct {
 		dsn string
 	}
 }
@@ -25,11 +30,39 @@ type application struct {
 	wg     sync.WaitGroup
 }
 
+func openDB(logger *jsonlog.Logger, cfg config) (*mongo.Client, error) {
+	client, err := mongo.NewClient(options.Client().ApplyURI(cfg.dbClient.dsn))
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			logger.LogFatal(err, nil)
+		}
+	}()
+
+	if err = client.Ping(ctx, readpref.Primary()); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
 func main() {
 	var cfg config
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development | staging | production)")
+	flag.StringVar(&cfg.dbClient.dsn, "db-dsn", "", "MongoDB_DSN")
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
@@ -39,7 +72,12 @@ func main() {
 		logger: logger,
 	}
 
-	err := app.serve()
+	_, err := openDB(app.logger, cfg)
+	if err != nil {
+		logger.LogFatal(err, nil)
+	}
+
+	err = app.serve()
 	if err != nil {
 		logger.LogFatal(err, nil)
 	}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/99designs/gqlgen v0.14.0
 	github.com/vektah/gqlparser/v2 v2.2.0
+	go.mongodb.org/mongo-driver v1.7.3 // indirect
 )
