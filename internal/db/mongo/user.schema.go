ec4d7004d58769809f6f0a4832ea6c0f60c32c3c
myko <awesomeme155@gmail.com>
2021-10-24 04:21:20 +0300

    feat: user route and endpoint
    - add /v1/users route path and endpoint

--- internal/db/mongo/user.schema.go
+++ internal/db/mongo/user.schema.go
@@ -6,37 +6,39 @@ import (
 )
 
 var userCollectionSchema = dbCollection{
-	"user": &options.CreateCollectionOptions{
+	"users": &options.CreateCollectionOptions{
 		ValidationAction: &validationAction,
 		ValidationLevel:  &validationLevel,
 		Validator: bson.M{
-			"bsonType": "object",
-			"required": []string{"username", "email", "password"},
-			"properties": bson.M{
-				"username": bson.M{
-					"bsonType":    "string",
-					"description": "user name",
-				},
-				"email": bson.M{
-					"bsonType":    "string",
-					"description": "user email address",
-				},
-				"password": bson.M{
-					"bsonType":    bson.TypeBinary,
-					"description": "user hashed password",
-				},
-				"isActive": bson.M{
-					"bsonType":    "bool",
-					"default":     "false",
-					"description": "user active status",
-				},
-				"version": bson.M{
-					"bsonType":    "string",
-					"description": "user record version since last update",
-				},
-				"created_at": bson.M{
-					"bsonType":    bson.TypeDateTime,
-					"description": "user record created date and time",
+			"$jsonSchema": bson.M{
+				"bsonType": "object",
+				"required": []string{"username", "email", "password"},
+				"additionalProperties": false,
+				"properties": bson.M{
+					"username": bson.M{
+						"bsonType":    "string",
+						"description": "user name",
+					},
+					"email": bson.M{
+						"bsonType":    "string",
+						"description": "user email address",
+					},
+					"password": bson.M{
+						"bsonType": "binData",
+						"description": "user hashed password",
+					},
+					"isActive": bson.M{
+						"bsonType":    "bool",
+						"description": "user active status",
+					},
+					"version": bson.M{
+						"bsonType":    "string",
+						"description": "user record version since last update",
+					},
+					"created_at": bson.M{
+						"bsonType":    "date",
+						"description": "user record created date and time",
+					},
 				},
 			},
 		},
